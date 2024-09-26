426dc829b5603926517084f98bc9c8c722667b8f
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-09-26 13:06:54 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(8, 12, 16, 18, 20);
+console.log(max);
